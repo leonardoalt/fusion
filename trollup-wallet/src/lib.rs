f020be2ac6c05193bd8a8cb546969e6581af73d6
Leo Alt <me@leoa.lt>
2023-02-06 22:02:26 +0100

    add signature and key tests

--- trollup-wallet/src/lib.rs
+++ trollup-wallet/src/lib.rs
@@ -1,7 +1,9 @@
-use ethers_core::types::{U256, U512};
+use ethers_core::types::U512;
 
 use trollup_api::{hash_tx, SignedTx, Tx};
-use trollup_types::{PrivateKey, ToBabyJubjubPoint, ToBabyJubjubSignature, ToBigInt};
+use trollup_types::{
+    FromBabyJubjubPoint, PrivateKey, PublicKey, ToBabyJubjubPoint, ToBabyJubjubSignature, ToBigInt,
+};
 
 pub fn sign(tx: &Tx, private_key: String) -> anyhow::Result<U512> {
     let wallet: PrivateKey = private_key.into();
@@ -28,6 +30,38 @@ pub fn new_private_key() -> PrivateKey {
     PrivateKey(babyjubjub_rs::new_key())
 }
 
-pub fn new_public_key(sk: &PrivateKey) -> U256 {
-    U256::from_big_endian(sk.0.public().compress().as_slice())
+pub fn new_public_key(sk: &PrivateKey) -> PublicKey {
+    PublicKey::from_babyjubjub_point(&sk.0.public())
+}
+
+pub fn new_key_pair() -> (PrivateKey, PublicKey) {
+    let sk = new_private_key();
+    let pk = new_public_key(&sk);
+    (sk, pk)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use trollup_types::ToU256;
+
+    #[test]
+    fn signatures() {
+        let (sk_1, pk_1) = new_key_pair();
+        let (_sk_2, pk_2) = new_key_pair();
+        let tx = Tx {
+            sender: pk_1.to_u256(),
+            to: pk_2.to_u256(),
+            nonce: 1.into(),
+            value: 0.into(),
+        };
+        let sig = sign(&tx, sk_1.to_string());
+        let mut signed_tx = SignedTx {
+            tx: tx,
+            signature: sig.unwrap().to_string(),
+        };
+        assert!(verify_tx_signature(&signed_tx).is_ok());
+        signed_tx.tx.nonce = 3.into();
+        assert!(verify_tx_signature(&signed_tx).is_err());
+    }
 }
